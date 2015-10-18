ae539ac455c098bf8359a0165c7004b20f7ebb08
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-10-18 15:56:27 +0200

    [travis] Add missing babel dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function({Plugin}) {
-  return new Plugin('react-remove-prop-types', {
+  return new Plugin('react-remove-properties', {
     visitor: {
       JSXIdentifier(node) {
         if (node.name === 'data-test') {
