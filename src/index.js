5eb52256abff0f469677fa5cea3a4dbd9b2f6f5c
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-12-24 18:09:17 +0100

    [ES5] ES6 preset is not installed

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@
 module.exports = function() {
   return {
     visitor: {
-      Program(path) {
-        // On program start, do an explicit traversal up front for your plugin.
+      Program: function(path) {
+        // On program start, do an explicit traversal up front for this plugin.
         path.traverse({
           JSXIdentifier: function(path) {
             if (path.node.name === 'data-test') {
               path.parentPath.remove();
             }
           },
-       });
+        });
       }
     },
   };
