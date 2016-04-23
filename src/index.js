7a744a5996bd8d6ed4ee610e2cefdb6cda8f2b1e
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-04-23 23:09:49 +0200

    Use the ES6 syntax

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-'use strict';
-
-module.exports = function() {
+export default function() {
   return {
     visitor: {
-      Program: function(path, state) {
+      Program(path, state) {
         // On program start, do an explicit traversal up front for this plugin.
+
         const property = state.opts.property || 'data-test';
         path.traverse({
-          JSXIdentifier: function(path) {
+          JSXIdentifier(path) {
             if (path.node.name === property) {
               path.parentPath.remove();
             }
