ffc4b63e613ff22c821b2c45dc2ea30176912ebd
quentin- <quentin@remind101.com>
2016-06-25 02:05:21 -0700

    add support for array option (#8)
    * add support for array option

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ export default function() {
     visitor: {
       Program(path, state) {
         // On program start, do an explicit traversal up front for this plugin.
+        const properties = state.opts.properties || [];
 
-        const property = state.opts.property || 'data-test';
+        if (state.opts.property) {
+          properties.push(state.opts.property);
+        }
+
+        if (properties.length === 0) {
+          properties.push('data-test');
+        }
+
         path.traverse({
           JSXIdentifier(path) {
-            if (path.node.name === property) {
+            if (properties.indexOf(path.node.name) > -1) {
               path.parentPath.remove();
             }
           },
---
+++ test/fixtures/array/expected.js
@@ -0,0 +1,11 @@
+"use strict";
+
+React.createClass({
+  render: function render() {
+    return React.createElement(
+      "div",
+      { "data-test": true },
+      "Hello Wold!"
+    );
+  }
+});
